13ed260fd0995263b817fda5493b0df83fa1fc3e
Jonathan Sofer <jdsofer@gmail.com>
2015-12-11 15:30:05 -0800

    Changed scaling numbers

--- www/js/boot.js
+++ www/js/boot.js
@@ -32,8 +32,8 @@ boot.prototype={
             this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
             this.scale.minWidth = gameWidth/2;
             this.scale.minHeight = gameHeight/2;
-            this.scale.maxWidth = 2048; //You can change this to gameWidth*2.5 if needed
-            this.scale.maxHeight = 1228; //Make sure these values are proportional to the gameWidth and gameHeight
+            this.scale.maxWidth = gameWidth*2.5; //2048
+            this.scale.maxHeight = gameHeight*2.5; //1228
             this.scale.pageAlignHorizontally = true;
             this.scale.pageAlignVertically = true;
             this.scale.forceOrientation(true, false);
